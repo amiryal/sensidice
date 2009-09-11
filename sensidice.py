58f95bfdd5f99916ca2eca5e8f5170df8fee26ad
Amir Yalon <amiryal@sensidice>
2009-09-11 12:24:47 +0300

    Create flat lists, return as joined strings.

--- sensidice.py
+++ sensidice.py
@@ -10,6 +10,14 @@ Copyright (c) 2009, Amir Yalon. All rights reserved.
 """
 
 
+def flatprepend(mutable_sequence, items):
+    if isinstance(items, list):
+        mutable_sequence[0:0] = items
+    else:
+        mutable_sequence.insert(0, items)
+    return mutable_sequence
+
+
 class DigitSeqCat:
 
     """Two sequences of digits concatenated dynamically.
@@ -54,9 +62,9 @@ class DigitSeqCartes:
     >>> len(dsq)
     30
     >>> dsq[0]
-    ('0', 'a')
+    ['0', 'a']
     >>> dsq[29]
-    ('4', 'f')
+    ['4', 'f']
     >>> dsq[31]
     ... #doctest: +IGNORE_EXCEPTION_DETAIL
     Traceback (most recent call last):
@@ -73,8 +81,11 @@ class DigitSeqCartes:
         return len(self.seq1) * len(self.seq2)
 
     def __getitem__(self, key):
-        len2 = len(self.seq2)
-        return (self.seq1[key // len2], self.seq2[key % len2])
+        lsbase = len(self.seq2)
+        res = []
+        flatprepend(res, self.seq2[key % lsbase])
+        flatprepend(res, self.seq1[key // lsbase])
+        return res
 
 
 class Digital:
@@ -83,22 +94,25 @@ class Digital:
     
     >>> d = Digital('0123456789')
     >>> d(0)
-    ['0']
+    '0'
     >>> d(9)
-    ['9']
+    '9'
     >>> d(10)
-    ['1', '0']
+    '1 0'
     >>> d = Digital('01') + Digital('ab') * Digital('AB')
     >>> d(0)
-    ['0']
+    '0'
     >>> d(1)
-    ['1']
+    '1'
     >>> d(2)
-    ['a', 'A']
+    'a A'
     >>> d(5)
-    ['b', 'B']
+    'b B'
     >>> d(6)
-    ['1', '0']
+    '1 0'
+    >>> d = Digital((0, 1)) * Digital(('a', 'b')) * Digital(['A', 'B'])
+    >>> d(0)
+    '0 a A'
     >>> d = Digital('01') + object()
     ... #doctest: +IGNORE_EXCEPTION_DETAIL
     Traceback (most recent call last):
@@ -119,15 +133,11 @@ class Digital:
         res = []
         base = len(self.digits)
         while True:
-            index = number % base
-            try:
-                res[0:0] = self.digits[index]
-            except TypeError:
-                res.insert(0, self.digits[index])
+            flatprepend(res, self.digits[number % base])
             if number < base:
                 break
             number //= base
-        return res
+        return " ".join([str(d) for d in res])
 
     def __add__(self, other):
         if not isinstance(other, Digital):
